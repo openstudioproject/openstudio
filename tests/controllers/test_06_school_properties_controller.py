84dda07767aed3644939263e5585df42fcf324d0
Edwin van de Ven <edwin@simplycode.nl>
2018-04-06 19:24:45 +0200

    Test teacher & employee delete

--- tests/controllers/test_06_school_properties_controller.py
+++ tests/controllers/test_06_school_properties_controller.py
@@ -6,6 +6,7 @@ These tests run based on webclient and need web2py server running.
 """
 from populate_os_tables import populate_tax_rates
 from populate_os_tables import populate_sys_organizations
+from populate_os_tables import populate_customers
 from populate_os_tables import populate_school_subscriptions
 from populate_os_tables import populate_school_subscriptions_groups
 from populate_os_tables import populate_school_classcards
@@ -13,6 +14,48 @@ from populate_os_tables import populate_school_classcards_groups
 
 
 from gluon.contrib.populate import populate
+
+
+
+def test_teacher_delete(client, web2py):
+    """
+        Can we remove the teacher status from a customer?
+    """
+    populate_customers(web2py)
+
+    row = web2py.db.auth_user(1001)
+    row.teacher = True
+    row.update_record()
+
+    web2py.db.commit()
+
+    url = '/school_properties/teacher_delete?uID=1001'
+    client.get(url)
+    assert client.status == 200
+
+    row = web2py.db.auth_user(1001)
+    assert row.teacher == False
+
+
+def test_employee_delete(client, web2py):
+    """
+        Can we remove the employee status from a customer?
+    """
+    populate_customers(web2py)
+
+    row = web2py.db.auth_user(1001)
+    row.employee = True
+    row.update_record()
+
+    web2py.db.commit()
+
+    url = '/school_properties/teacher_delete?uID=1001'
+    client.get(url)
+    assert client.status == 200
+
+    row = web2py.db.auth_user(1001)
+    assert row.teacher == False
+
 
 def test_school_subscriptions_index_and_current_price(client, web2py):
     """
