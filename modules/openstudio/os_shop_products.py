79158e151dc13f80003fb9063192600d16d9076f
Edwin van de Ven <edwin@simplycode.nl>
2018-06-05 15:11:04 +0200

    Moved class ShopProducts to separate file

---
+++ modules/openstudio/os_shop_products.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+
+from gluon import *
+
+
+class ShopProducts:
+    def list(self):
+        """
+            :return: List of shop products (gluon.dal.rows)
+        """
+        db = current.globalenv['db']
+
+        rows = db(db.shop_products).select(db.shop_products.ALL,
+                                           orderby=db.shop_products.Name)
+
+        return rows
+
+
+    def list_formatted(self):
+        """
+            :return: HTML table with shop products
+        """
+        T = current.globalenv['T']
+        os_gui = current.globalenv['os_gui']
+        auth = current.globalenv['auth']
+
+        header = THEAD(TR(TH(),
+                          TH(T('Name')),
+                          TH(T('Description')),
+                          TH()))
+        table = TABLE(header, _class='table table-striped table-hover')
+
+        permission_variants = (auth.has_membership(group_id='Admins') or
+                               auth.has_permission('read', 'shop_products_variants'))
+        permission_edit = (auth.has_membership(group_id='Admins') or
+                           auth.has_permission('update', 'shop_products'))
+        permission_delete = (auth.has_membership(group_id='Admins') or
+                             auth.has_permission('delete', 'shop_products'))
+
+        onclick_delete = "return confirm('" \
+            + T('Do you really want to delete this product?') + "');"
+
+        rows = self.list()
+        for i, row in enumerate(rows):
+            repr_row = list(rows[i:i + 1].render())[0]
+            buttons = DIV(_class='pull-right')
+            vars = {'spID':row.id}
+
+            if permission_variants:
+                variants = os_gui.get_button('noicon',
+                    URL('shop_manage', 'product_variants', vars=vars),
+                    title=T('Variants'))
+                buttons.append(variants)
+            if permission_edit:
+                edit = os_gui.get_button('edit',
+                    URL('shop_manage', 'product_edit', vars=vars))
+                buttons.append(edit)
+            if permission_delete:
+                delete = os_gui.get_button('delete_notext',
+                    URL('shop_manage', 'product_delete', vars=vars),
+                    onclick=onclick_delete)
+                buttons.append(delete)
+
+            tr = TR(
+                TD(repr_row.thumbsmall),
+                TD(os_gui.max_string_length(row.Name, 30)),
+                TD(os_gui.max_string_length(row.Description, 30)),
+                TD(buttons)
+            )
+
+            table.append(tr)
+
+        return table
