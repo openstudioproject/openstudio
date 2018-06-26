499969fd76d349c9f985b2c6b3f0ec720102523c
Edwin van de Ven <edwin@simplycode.nl>
2018-06-26 18:15:26 +0200

    hello world!

---
+++ controllers/pos.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from general_helpers import set_form_id_and_get_submit_button
+
+def index():
+    return dict(message='hello world')
---
+++ react_frontend_src/pos/src/components/App.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react"
+import { render } from "react-dom"
+
+const App = () =>
+        <div>hello world!</div>
+
+export default App
