f80bf206540a9e6257f203fc14da28997cf89d61
Edwin van de Ven <edwin@simplycode.nl>
2018-06-27 21:15:24 +0200

    No more erros

--- react_frontend_src/pos/src/components/MainMenu.js
+++ react_frontend_src/pos/src/components/MainMenu.js
@@ -15,7 +15,7 @@ const MainMenu = () =>
             <NavbarHeader />
             <NavbarCollapse>
                 <NavbarNav>
-                    <li><NavLink to="/" index={true} activeClassName={activeClassName}>Check-in</NavLink></li>
+                    <li><NavLink to="/" activeClassName={activeClassName}>Check-in</NavLink></li>
                     <li><NavLink to="/products" activeClassName={activeClassName}>Point of Sale</NavLink></li>
                 </NavbarNav>                
             </NavbarCollapse>
