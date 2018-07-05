d5df73514c4096642907f35a53eedbb74163304d
Edwin van de Ven <edwin@simplycode.nl>
2018-07-05 19:51:06 +0200

    Fix initial content-wrapper height

--- react_frontend_src/pos/src/components/ui/Content.js
+++ react_frontend_src/pos/src/components/ui/Content.js
@@ -2,7 +2,10 @@ import React from "react"
 import ContentHeader from './ContentHeader'
 
 const initialStyle = {
-    minHeight: window.innerHeight
+    // The AdminLTE function handling this doens't seem to be kicked off.
+    // so we're setting the height of the content-wrapper manually
+    // 50 is the Height of the navigation header
+    minHeight: window.innerHeight - 50
 }
 
 const Content = ({title, children}) =>
