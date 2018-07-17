b1992aa25b97255537df3a60dcb8883f360b1277
Edwin van de Ven <edwin@simplycode.nl>
2018-07-17 12:59:16 +0200

    Added os_classes_reservation stub

---
+++ modules/openstudio/os_classes_reservation.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from gluon import *
+
+
+class ClassesReservation:
+    """
+        This class collects functions for a class reservation
+    """
+    def __init__(self):
+        pass
