1ee6b93bca26bd825fcf6b66af59844e75cf252d
Unknown <ferric.danavis@gmail.com>
2018-08-23 11:51:47 +0200

    typo and description for registration fee

--- models/db_tables.py
+++ models/db_tables.py
@@ -1274,12 +1274,16 @@ def define_school_subscriptions():
               label=T('Quick Stats Amount'),
               default=0,
               comment=os_gui.get_info_icon(
-                  title=T("As for subscription it's impossible to know the exact revenue for each class until the of the month. This amount will be used to create rough estimates of class revenue."),
+                  title=T("As for subscription it's impossible to know the exact revenue for each class until the end of the month. This amount will be used to create rough estimates of class revenue."),
                   btn_icon='info')),
         Field('RegistrationFee', 'double',
               label=T('Registration Fee'),
               default=0,
-              required=False),
+              comment=os_gui.get_info_icon(
+                  title=T(
+                      "This Amount will be substracted from the first subscription made for this customer, if there should be no RegistrationFee, just leave this field empty "),
+                  btn_icon='info')),
+
         format=format)
 
 
