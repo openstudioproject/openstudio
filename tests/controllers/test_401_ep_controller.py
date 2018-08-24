60fd1a41c5db2b0f573feb97bfe8d6c998a4a302
Unknown <ferric.danavis@gmail.com>
2018-08-24 17:23:52 +0200

    working migration for ep/my_payments tests

--- tests/controllers/test_401_ep_controller.py
+++ tests/controllers/test_401_ep_controller.py
@@ -1,18 +1,17 @@
-def test_staff_payments_link_not_shown(client, web2py):
-    """
-        The staff payments link shouldn't show
-    """
-    from setup_profile_tests import setup_profile_tests
-    setup_profile_tests(web2py)
+#!/usr/bin/env python
 
-    url = '/profile/index'
-    client.get(url)
-    assert client.status == 200
+"""
+    py.test test cases to test the ep controller (ep.py)
+"""
 
-    assert "Payments not in client.text"
+from gluon.contrib.populate import populate
+
+from populate_os_tables import populate_auth_user_teachers
+from setup_profile_tests import setup_profile_tests
 
 
-def test_staff_payments(client, web2py):
+
+def test_my_payments(client, web2py):
     """
         Is the staff payments page showing?
     """
@@ -40,10 +39,24 @@ def test_staff_payments(client, web2py):
     assert client.status == 200
 
     # Check payments display
-    url = '/profile/staff_payments'
+    url = '/ep/my_payments'
     client.get(url)
     assert client.status == 200
 
     ic = web2py.db.invoices_customers(auth_customer_id=300)
     invoice = web2py.db.invoices(ic.invoices_id)
     assert invoice.InvoiceID in client.text
+
+
+def test_my_payments_link_not_shown(client, web2py):
+    """
+        The staff payments link shouldn't show
+    """
+
+    populate_auth_user_teachers(web2py)
+
+    url = '/ep/index'
+    client.get(url)
+    assert client.status == 200
+
+    assert "Payments not in client.text"
