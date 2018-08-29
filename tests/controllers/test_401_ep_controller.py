7a71155e504908e550ae203f6245f625d8d84b6e
Edwin van de Ven <edwin@simplycode.nl>
2018-08-29 18:27:39 +0200

    Test my payments

--- tests/controllers/test_401_ep_controller.py
+++ tests/controllers/test_401_ep_controller.py
@@ -153,45 +153,13 @@ def test_my_payments(client, web2py):
     assert client.status == 200
 
     setup_ep_tests(web2py)
-
-    au = web2py.db.auth_user(300)
-    au.teacher = True
-    au.update_record()
-
-    web2py.db.commit()
-
     populate_auth_user_teachers_payment_invoices(web2py)
-
-    # log out and log back in again to make the profile user a teacher
-    url = '/default/user/logout'
-    client.get(url)
-    assert client.status == 200
-
-    data = dict(email='ep@openstudioproject.com',
-                password='password',
-                _formname='login')
-    client.post('/default/user/login', data=data)
-    assert client.status == 200
 
     # Check payments display
     url = '/ep/my_payments'
     client.get(url)
     assert client.status == 200
 
-    ic = web2py.db.invoices_customers(auth_customer_id=300)
+    ic = web2py.db.invoices_customers(auth_customer_id=400)
     invoice = web2py.db.invoices(ic.invoices_id)
     assert invoice.InvoiceID in client.text
-
-
-def test_my_payments_link_not_shown(client, web2py):
-    """
-        The staff payments link shouldn't show
-    """
-
-    populate_auth_user_teachers(web2py)
-    setup_ep_tests(web2py)
-    url = '/ep/index'
-    client.get(url)
-    assert client.status == 200
-
-    assert "Payments" not in client.text
