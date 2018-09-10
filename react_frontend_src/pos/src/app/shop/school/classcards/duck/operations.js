d957fb0ed5094cd07f27402cc40739d09ddd171c
Edwin van de Ven <edwin@simplycode.nl>
2018-09-10 08:36:40 +0200

    Operations

--- react_frontend_src/pos/src/app/shop/school/classcards/duck/operations.js
+++ react_frontend_src/pos/src/app/shop/school/classcards/duck/operations.js
@@ -1,36 +1,23 @@
 import {
-    requestCheckinClassAttendance as request_class_attendance,
-    receiveCheckinClassAttendance as receive_class_attendance,
-    setCheckinClassAttendanceSearchCustomerID as set_customer_id,
-    setCheckinSearchTimeout as set_search_timeout,
-    clearCheckinSearchTimeout as clear_search_timeout
+    requestShopSchoolClasscards as request_classcards,
+    receiveShopSchoolClasscards as receive_classcards,
 } from './actions'
 
-import axios_os from '../../../../utils/axios_os'
-import OS_API from '../../../../utils/os_api'
-import { toISODate } from '../../../../utils/date_tools'
+import axios_os from '../../../../../utils/axios_os'
+import OS_API from '../../../../../utils/os_api'
 
 // just pass these actions as there's nothing else they need to do
 // Put pass-through actions here
-const setCheckinClassAttendanceSearchCustomerID = set_customer_id
-const clearCheckinSearchTimeout = clear_search_timeout
-const setCheckinSearchTimeout = set_search_timeout
 
 // data fetchers
-const fetchClassAttendance = (clsID) => {
+const fetchShopClasscards = () => {
       return dispatch => {
-          dispatch(request_class_attendance())
+          dispatch(request_classclasscards())
 
-          const date = new Date()
-          const iso_date = toISODate(date)
-          const params = new URLSearchParams()
-          params.append('clsID', clsID)
-          params.append('date', iso_date)
-          console.log(params)
-          axios_os.post(OS_API.CHECKIN_ATTENDANCE, params)
+          axios_os.get(OS_API.SHOP_SCHOOL_CLASSCARDS)
           .then(function (response) {
             // handle success
-            dispatch(receive_class_attendance(response.data))
+            dispatch(receive_classcards(response.data))
             // dispatch(setLoadingProgress(100))
           })
           .catch(function (error) {
@@ -45,8 +32,5 @@ const fetchClassAttendance = (clsID) => {
 
 
 export default {
-    fetchClassAttendance,
-    setCheckinClassAttendanceSearchCustomerID,
-    setCheckinSearchTimeout,
-    clearCheckinSearchTimeout
+    fetchShopClasscards
 }
