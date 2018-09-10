de41c3a40d0e3f29ae8883dfeb6ad6d539074fff
Edwin van de Ven <edwin@simplycode.nl>
2018-09-10 08:28:23 +0200

    Reducers

--- react_frontend_src/pos/src/app/shop/school/classcards/duck/reducers.js
+++ react_frontend_src/pos/src/app/shop/school/classcards/duck/reducers.js
@@ -1,39 +1,23 @@
 import T from './types'
 
-export const checkinAttendanceReducer = (state = {}, action={ type: null }) => {
+export const shopSchoolClasscardsReducer = (state = {}, action={ type: null }) => {
     switch (action.type) {
-        case T.CHECKIN_SET_CLASS_ATTENDANCE_LOADING:
+        case T.SHOP_SCHOOL_SET_CLASSCARDS_LOADING:
             return {
                 ...state,
                 loading: action.loading,
             }
-        case T.CHECKIN_REQUEST_CLASS_ATTENDANCE:
+        case T.SHOP_SCHOOL_REQUEST_CLASSCARDS:
             return {
                 loading: true,
                 loaded: false,
                 data: {}
             }
-        case T.CHECKIN_RECEIVE_CLASS_ATTENDANCE:
+        case T.SHOP_SCHOOL_RECEIVE_CLASSCARDS:
             return {
                 loading: false,
                 loaded: true,
-                data: action.data.attendance,
-            }
-        case T.CHECKIN_SET_CLASS_ATTENDANCE_SEARCH_CUSTOMER_ID:
-            return {
-                ...state,
-                search_customer_id: action.search_customer_id,
-            }
-        case T.CHECKIN_CLEAR_SEARCH_TIMEOUT:
-            return {
-                ...state,
-                searchTimeout: clearTimeout(state.searchTimeout),
-                
-            }
-        case T.CHECKIN_SET_SEARCH_TIMEOUT:
-            return {
-                ...state,
-                searchTimeout: action.timeout,
+                data: action.data,
             }
         default:
             return {
