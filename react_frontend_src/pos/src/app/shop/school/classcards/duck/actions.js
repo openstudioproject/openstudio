c147901d0fce86f6d86e87b082b99d01bbd8dae6
Edwin van de Ven <edwin@simplycode.nl>
2018-09-10 08:31:08 +0200

    Actions

--- react_frontend_src/pos/src/app/shop/school/classcards/duck/actions.js
+++ react_frontend_src/pos/src/app/shop/school/classcards/duck/actions.js
@@ -1,42 +1,25 @@
 import T from './types'
 
 
-export const requestCheckinClassAttendance = () =>
+export const requestShopSchoolClasscards = () =>
     ({
-        type: T.CHECKIN_REQUEST_CLASS_ATTENDANCE
+        type: T.SHOP_SCHOOL_REQUEST_CLASSCARDS
     })
 
-export const receiveCheckinClassAttendance = (data) =>
+export const receiveShopSchoolClasscards = (data) =>
     ({
-        type: T.CHECKIN_RECEIVE_CLASS_ATTENDANCE,
+        type: T.SHOP_SCHOOL_RECEIVE_CLASSCARDS,
         data
     })
 
-export const setCheckinClassAttendanceLoaded = (loaded) =>
+export const setShopSchoolClasscardsLoaded = (loaded) =>
     ({
-        type: T.CHECKIN_SET_CLASS_ATTENDANCE_LOADED,
+        type: T.SHOP_SCHOOL_SET_CLASSCARDS_LOADED,
         loaded
     })
 
-export const setCheckinClassAttendanceLoading = (loading) =>
+export const setShopSchoolClasscardsLoading = (loading) =>
     ({
-        type: T.CHECKIN_SET_CLASS_ATTENDANCE_LOADING,
+        type: T.SHOP_SCHOOL_SET_CLASSCARDS_LOADING,
         loading
-    })
-
-export const setCheckinClassAttendanceSearchCustomerID = (search_id) =>
-    ({
-        type: T.CHECKIN_SET_CLASS_ATTENDANCE_SEARCH_CUSTOMER_ID,
-        search_id,
-    })
-
-export const clearCheckinSearchTimeout = () =>
-    ({
-        type: T.CHECKIN_CLEAR_SEARCH_TIMEOUT
-    })
-
-export const setCheckinSearchTimeout = (timeout) =>
-    ({
-        type: T.CHECKIN_SET_SEARCH_TIMEOUT,
-        timeout,
     })
