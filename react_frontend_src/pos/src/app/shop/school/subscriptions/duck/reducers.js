5159bd3cd7c70eb48e2b239a833fd7322c5c3846
Edwin van de Ven <edwin@simplycode.nl>
2018-09-12 13:29:15 +0200

    reducers

--- react_frontend_src/pos/src/app/shop/school/subscriptions/duck/reducers.js
+++ react_frontend_src/pos/src/app/shop/school/subscriptions/duck/reducers.js
@@ -1,19 +1,19 @@
 import T from './types'
 
-export const shopSchoolClasscardsReducer = (state = {}, action={ type: null }) => {
+export const shopSchoolSubscriptionsReducer = (state = {}, action={ type: null }) => {
     switch (action.type) {
-        case T.SHOP_SCHOOL_SET_CLASSCARDS_LOADING:
+        case T.SHOP_SCHOOL_SET_SUBSCRIPTIONS_LOADING:
             return {
                 ...state,
                 loading: action.loading,
             }
-        case T.SHOP_SCHOOL_REQUEST_CLASSCARDS:
+        case T.SHOP_SCHOOL_REQUEST_SUBSCRIPTIONS:
             return {
                 loading: true,
                 loaded: false,
                 data: {}
             }
-        case T.SHOP_SCHOOL_RECEIVE_CLASSCARDS:
+        case T.SHOP_SCHOOL_RECEIVE_SUBSCRIPTIONS:
             return {
                 loading: false,
                 loaded: true,
