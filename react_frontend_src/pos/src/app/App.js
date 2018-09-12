91b13375195a419ff5b858d1e6df852efbbcb9c1
Edwin van de Ven <edwin@simplycode.nl>
2018-09-12 13:37:43 +0200

    Route for subscriptions

--- react_frontend_src/pos/src/app/App.js
+++ react_frontend_src/pos/src/app/App.js
@@ -13,6 +13,7 @@ import Book from './checkin/book/BookContainer'
 import Revenue from './checkin/revenue/RevenueContainer'
 import Home from './home/HomeContainer'
 import Classcards from './shop/school/classcards/ClasscardsContainer'
+import Classcards from './shop/school/subscriptions/SubscriptionsContainer'
 import PermissionsError from './permissions_error/PermissionsErrorContainer'
 import Whoops404 from './whoops404/Whoops404'
 import LoadingScreen from '../components/ui/LoadingScreen'
@@ -45,6 +46,7 @@ class App extends Component {
             <Route path='/checkin/book/:clsID/:cuID' component={Book} />
             <Route path='/checkin/revenue/:clsID' component={Revenue} />
             <Route path='/products/school/classcards' component={Classcards} />
+            <Route path='/products/school/Subscriptions' component={Subscriptions} />
             <Route path='/permissions_error' component={PermissionsError} />
             {/* Add all your remaining routes here, like /trending, /about, etc. */}
             <Route component={Whoops404} />
