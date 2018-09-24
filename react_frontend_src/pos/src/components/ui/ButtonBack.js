be888c6f4d18051c74e20d0af6c01c44c56f4035
Edwin van de Ven <edwin@simplycode.nl>
2018-09-24 12:39:39 +0200

    ButtonBack component

---
+++ react_frontend_src/pos/src/components/ui/ButtonBack.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const ButtonBack = ({children}) =>
+    <button className="btn btn-default">
+        <i className="fa fa-chevron-left"></i>
+        {children}
+    </button>
+
+export default Box
