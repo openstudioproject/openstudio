d39afbdff680d61a6ee9f61946dd1ade47bbd4eb
Edwin van de Ven <edwin@simplycode.nl>
2018-09-25 13:14:41 +0200

    merge conflict resolved

--- react_frontend_src/pos/src/app/customers/list/CustomersContainer.js
+++ react_frontend_src/pos/src/app/customers/list/CustomersContainer.js
@@ -10,11 +10,7 @@ import { customersListOperations } from './duck'
 const mapStateToProps = state => 
     ({
         app: state.app,
-<<<<<<< HEAD
         barcode_scans: state.app.settings.data.customers_barcodes,
-=======
-        barcode_scans: state.app.settings.data.checkin_barcodes,
->>>>>>> pos_customers
         customers: state.customers.list
     })
 
