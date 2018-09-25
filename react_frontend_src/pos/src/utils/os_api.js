8c74c76eaee5b517abff0d092f83f3c1f0551c60
Edwin van de Ven <edwin@simplycode.nl>
2018-09-25 11:20:46 +0200

    customer memberships types

--- react_frontend_src/pos/src/utils/os_api.js
+++ react_frontend_src/pos/src/utils/os_api.js
@@ -9,6 +9,7 @@ const api_urls = {
     'CHECKIN_TEACHER_PAYMENT': '/pos/get_class_teacher_payment.json',
     'CHECKIN_VERIFY_TEACHER_PAYMENT': '/pos/verify_teacher_payment.json',
     'CUSTOMERS': '/pos/get_customers.json',
+    'CUSTOMERS_MEMBERSHIPS': '/pos/get_customers_memberships.json',
     'SHOP_SCHOOL_CLASSCARDS': '/pos/get_school_classcards.json',
     'SHOP_SCHOOL_MEMBERSHIPS': '/pos/get_school_memberships.json',
     'SHOP_SCHOOL_SUBSCRIPTIONS': '/pos/get_school_subscriptions.json'
