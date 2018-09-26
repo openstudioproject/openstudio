866c40ef896bf166c211829e7f9a00c3554c6237
Edwin van de Ven <edwin@simplycode.nl>
2018-09-26 16:04:16 +0200

    Add displayID to initial state

--- react_frontend_src/pos/src/components/ui/InputGroupSearch.js
+++ react_frontend_src/pos/src/components/ui/InputGroupSearch.js
@@ -1,16 +1,26 @@
 import React from "react"
 
+// const clear = () => {
+//     const input = document.getElementById('InputSearch')
+//     input.value = ''
+//     const ev = new Event('input', { bubbles: true})
+//     input.dispatchEvent(ev)
+// }
+
 const InputGroupSearch = ({placeholder, onChange=f=>f}) => 
     <div className="input-group">
         <span className="input-group-addon">
             <i className="fa fa-search"></i>
         </span>
         <input type="text"
+               id="InputSearch"
                className="form-control"
                placeholder={placeholder} 
                onChange={onChange}
                ref={input => input && input.focus()} />
-            {/* placeholder="Search..." /> */}
+        {/* <span className="input-group-addon" onClick={clear}>
+            <i className="fa fa-times"></i>
+        </span> */}
     </div>
 
 
