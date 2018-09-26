e0f729622b23b4d6b994dd262f993ba9277b8631
Edwin van de Ven <edwin@simplycode.nl>
2018-09-26 16:10:34 +0200

    Improved InputGroupSearch

--- react_frontend_src/pos/src/components/ui/InputGroupSearch.js
+++ react_frontend_src/pos/src/components/ui/InputGroupSearch.js
@@ -1,26 +1,21 @@
 import React from "react"
 
-// const clear = () => {
-//     const input = document.getElementById('InputSearch')
-//     input.value = ''
-//     const ev = new Event('input', { bubbles: true})
-//     input.dispatchEvent(ev)
-// }
 
-const InputGroupSearch = ({placeholder, onChange=f=>f}) => 
+const InputGroupSearch = ({value, placeholder, onChange=f=>f, onClear=f=>f}) => 
     <div className="input-group">
         <span className="input-group-addon">
             <i className="fa fa-search"></i>
         </span>
         <input type="text"
                id="InputSearch"
+               value={value}
                className="form-control"
                placeholder={placeholder} 
                onChange={onChange}
                ref={input => input && input.focus()} />
-        {/* <span className="input-group-addon" onClick={clear}>
+        <span className="input-group-addon" onClick={onClear}>
             <i className="fa fa-times"></i>
-        </span> */}
+        </span>
     </div>
 
 
