85be5653da154b99782c7aaff18a2a8f4a5eef0f
Edwin van de Ven <edwin@simplycode.nl>
2018-10-18 18:49:33 +0200

    Code cleanup

--- modules/openstudio/os_shop_products_variants.py
+++ modules/openstudio/os_shop_products_variants.py
@@ -58,35 +58,6 @@ class ShopProductsVariants:
             })
 
 
-
-
-
-        # db = current.db
-        #
-        # products_with_variants = []
-        #
-        # product_rows = self.get_products()
-        #
-        # for product_row in product_rows:
-        #     query = (db.shop_products_variants.shop_products_id == self.id)
-        #
-        #     rows = db(query).select(
-        #         db.shop_products_variants.ALL,
-        #         orderby=db.shop_products_variants.Name,
-        #     )
-        #
-        #     products_with_variants.append({
-        #         'id': product_row.id,
-        #         'name': product_row.Name,
-        #         'description': product_row.DescriptionShop,
-        #         'picture': product_row.picture,
-        #         'thumbsmall': get_download_url(product_row.thumbsmall),
-        #         'thumblarge': get_download_url(product_row.thumblarge),
-        #         'variants': rows.as_list(),
-        #     })
-
-
-
         return data
 
 
