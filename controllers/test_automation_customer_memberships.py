63e5544dbbb9e3d724c671c86e289db5134952f6
Edwin van de Ven <edwin@simplycode.nl>
2018-10-29 13:54:06 +0100

    Moved customer memberships test function to own controller

--- controllers/test_automation_customer_memberships.py
+++ controllers/test_automation_customer_memberships.py
@@ -4,22 +4,19 @@ from openstudio.os_scheduler_tasks import OsSchedulerTasks
 
 
 @auth.requires(auth.user_id == 1)
-def test_create_invoices():
+def test_check_memberships_subscriptions():
     """
     Function to expose class & method used by scheduler task
     to create monthly invoices
     """
-
     if ( not web2pytest.is_running_under_test(request, request.application)
          and not auth.has_membership(group_id='Admins') ):
         redirect(URL('default', 'user', args=['not_authorized']))
-
 
 
     ost = OsSchedulerTasks()
 
     year = request.vars['year']
     month = request.vars['month']
-    description = request.vars['description']
 
-    ost.customers_subscriptions_create_invoices_for_month(year, month, description)
+    ost.customers_memberships_renew_expired(year, month)
