eff2f80f1bd175b57291745cb77075db0bab3bce
Edwin van de Ven <edwin@simplycode.nl>
2018-10-30 16:17:06 +0100

    Add cart icon

--- react_frontend_src/pos/src/app/shop/cart/Cart.js
+++ react_frontend_src/pos/src/app/shop/cart/Cart.js
@@ -29,7 +29,7 @@ class Products extends Component {
             <div className="box box-solid"> 
                 <div className="box-header">
                     <h3 className="box-title">
-                        Shopping cart
+                        <i className="fa fa-shopping-cart"></i> Cart
                     </h3>
                 </div>
                 <div className="box-body">
