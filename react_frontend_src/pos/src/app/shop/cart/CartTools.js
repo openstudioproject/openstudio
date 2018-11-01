00abeac7a99619039ab36dbd236510af0e217b97
Edwin van de Ven <edwin@simplycode.nl>
2018-11-01 14:14:47 +0100

    Placeholder for payment button

--- react_frontend_src/pos/src/app/shop/cart/CartTools.js
+++ react_frontend_src/pos/src/app/shop/cart/CartTools.js
@@ -9,7 +9,6 @@ const CartTools = ({customers, deleteSelectedItem, cart_selected_item}) =>
         <div className="box-body">
             <div className="col-md-6">
                 <CartToolsCustomer customers={customers} />
-                Customer & Payment
             </div>
             <div className="col-md-6">
                 {console.log('cart_selected_item')}
@@ -17,8 +16,11 @@ const CartTools = ({customers, deleteSelectedItem, cart_selected_item}) =>
                 <button className="btn btn-default btn-block"
                         disabled={(!cart_selected_item.length)}
                         onClick={ () => deleteSelectedItem() }>
-                    Delete item
+                    Delete
                 </button>
+            </div>
+            <div className="col-md-12">
+                Payment button here
             </div>
         </div>
     </div>
