314ebfe5a173e9ea5d7c944f7552b53f8eef3b3d
Edwin van de Ven <edwin@simplycode.nl>
2018-11-30 12:59:46 +0100

    Add basic html

--- react_frontend_src/pos/src/app/customers/list/CustomerDisplay.js
+++ react_frontend_src/pos/src/app/customers/list/CustomerDisplay.js
@@ -14,9 +14,29 @@ const CustomerDisplay = ({customerID, customers, edit_in_progress, onClickEdit=f
                 <h3 className="box-title">Customer</h3>
             </div>
             <div className="box-body">
-                {customers[customerID].display_name}
-                {customers[customerID].address}
-                <ButtonCustomerEdit onClick={onClickEdit}/>
+                <div className="col-md-6">
+                    <div class="app">
+                        <a href="#" id="start-camera" class="visible">Touch here to start the app.</a>
+                        <video id="camera-stream"></video>
+                        <img id="snap" />
+
+                        <p id="error-message"></p>
+
+                        <div class="controls">
+                          <a href="#" id="delete-photo" title="Delete Photo" class="disabled"><i class="material-icons">delete</i></a>
+                            <a href="#" id="take-photo" title="Take Photo"><i class="material-icons">camera_alt</i></a>
+                            <a href="#" id="download-photo" download="selfie.png" title="Save Photo" class="disabled"><i class="material-icons">file_download</i></a>  
+                        </div>
+
+                        {/* <!-- Hidden canvas element. Used for taking snapshot of video. --> */}
+                        <canvas></canvas>
+                    </div>
+                </div>
+                <div className="col-md-6">
+                    {customers[customerID].display_name}
+                    {customers[customerID].address}
+                    <ButtonCustomerEdit onClick={onClickEdit}/>
+                </div>
             </div>
         </div>
         }
