3e5082886fe99655311a2327b81d5e8462bfd9db
Edwin van de Ven <edwin@simplycode.nl>
2018-11-30 12:26:05 +0100

    Cancel button respects redirect back to

--- react_frontend_src/pos/src/app/customers/list/Customers.js
+++ react_frontend_src/pos/src/app/customers/list/Customers.js
@@ -137,7 +137,12 @@ class Customers extends Component {
 
     onClickButtonBack(e) {
         console.log("clicked")
-        this.props.history.push('/shop/products')
+        const next_component = this.props.customers.redirect_next_component
+        if (next_component) {
+            this.props.history.push(next_component)
+        } else {
+            this.props.history.push('/shop/products')
+        }
     }
 
     onCreateCustomer(e) {
