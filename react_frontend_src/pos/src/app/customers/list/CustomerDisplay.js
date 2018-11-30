bf05e3a1783560d657bb08da579e76f4194df501
Edwin van de Ven <edwin@simplycode.nl>
2018-11-30 13:23:46 +0100

    Rewrite CustomerDisplay as regular component

--- react_frontend_src/pos/src/app/customers/list/CustomerDisplay.js
+++ react_frontend_src/pos/src/app/customers/list/CustomerDisplay.js
@@ -6,41 +6,105 @@ import { v4 } from "uuid"
 
 import ButtonCustomerEdit from "../../../components/ui/ButtonCustomerEdit"
 
-const CustomerDisplay = ({customerID, customers, edit_in_progress, onClickEdit=f=>f}) => 
-    <div>
-        { !(customerID) || (edit_in_progress) ? null :
-        <div className="box box-solid"> 
-            <div className="box-header">
-                <h3 className="box-title">Customer</h3>
-            </div>
-            <div className="box-body">
-                <div className="col-md-6">
-                    <div class="app">
-                        <a href="#" id="start-camera" class="visible">Touch here to start the app.</a>
-                        <video id="camera-stream"></video>
-                        <img id="snap" />
 
-                        <p id="error-message"></p>
+class CustomerDisplay extends Component {
+    constructor(props) {
+        super(props)
+        console.log("Customer display props:")
+        console.log(props)
+    }
 
-                        <div class="controls">
-                          <a href="#" id="delete-photo" title="Delete Photo" class="disabled"><i class="material-icons">delete</i></a>
-                            <a href="#" id="take-photo" title="Take Photo"><i class="material-icons">camera_alt</i></a>
-                            <a href="#" id="download-photo" download="selfie.png" title="Save Photo" class="disabled"><i class="material-icons">file_download</i></a>  
+    PropTypes = {
+        intl: intlShape.isRequired,
+        customerID: PropTypes.integer,
+        customers: PropTypes.object,
+        edit_in_progress: PropTypes.boolean,
+        onClickEdit: PropTypes.function
+    }
+
+    render() {
+        const customerID = this.props.customerID
+        const customers = this.props.customers
+        const edit_in_progress = this.props.edit_in_progress
+        const onClickEdit = this.props.onClickEdit
+
+        return (
+            <div>
+                { !(customerID) || (edit_in_progress) ? null :
+                <div className="box box-solid"> 
+                    <div className="box-header">
+                        <h3 className="box-title">Customer</h3>
+                    </div>
+                    <div className="box-body">
+                        <div className="col-md-6">
+                            <div className="camera-app">
+                                <a href="#" id="start-camera" className="visible">Touch here to start the app.</a>
+                                <video id="camera-stream"></video>
+                                <img id="snap" />
+
+                                <p id="error-message"></p>
+
+                                <div className="controls">
+                                <a href="#" id="delete-photo" title="Delete Photo" className="disabled"><i className="material-icons">delete</i></a>
+                                    <a href="#" id="take-photo" title="Take Photo"><i className="material-icons">camera_alt</i></a>
+                                    <a href="#" id="download-photo" download="selfie.png" title="Save Photo" className="disabled"><i className="material-icons">file_download</i></a>  
+                                </div>
+
+                                {/* <!-- Hidden canvas element. Used for taking snapshot of video. --> */}
+                                <canvas></canvas>
+                            </div>
                         </div>
-
-                        {/* <!-- Hidden canvas element. Used for taking snapshot of video. --> */}
-                        <canvas></canvas>
+                        <div className="col-md-6">
+                            {customers[customerID].display_name}
+                            {customers[customerID].address}
+                            <ButtonCustomerEdit onClick={onClickEdit}/>
+                        </div>
                     </div>
                 </div>
-                <div className="col-md-6">
-                    {customers[customerID].display_name}
-                    {customers[customerID].address}
-                    <ButtonCustomerEdit onClick={onClickEdit}/>
-                </div>
+                }
             </div>
-        </div>
-        }
-    </div>
-
+        )
+    }
+}
 
 export default CustomerDisplay
+
+
+// const CustomerDisplay = ({customerID, customers, edit_in_progress, onClickEdit=f=>f}) => 
+//     <div>
+//         { !(customerID) || (edit_in_progress) ? null :
+//         <div className="box box-solid"> 
+//             <div className="box-header">
+//                 <h3 className="box-title">Customer</h3>
+//             </div>
+//             <div className="box-body">
+//                 <div className="col-md-6">
+//                     <div className="app">
+//                         <a href="#" id="start-camera" className="visible">Touch here to start the app.</a>
+//                         <video id="camera-stream"></video>
+//                         <img id="snap" />
+
+//                         <p id="error-message"></p>
+
+//                         <div className="controls">
+//                           <a href="#" id="delete-photo" title="Delete Photo" className="disabled"><i className="material-icons">delete</i></a>
+//                             <a href="#" id="take-photo" title="Take Photo"><i className="material-icons">camera_alt</i></a>
+//                             <a href="#" id="download-photo" download="selfie.png" title="Save Photo" className="disabled"><i className="material-icons">file_download</i></a>  
+//                         </div>
+
+//                         {/* <!-- Hidden canvas element. Used for taking snapshot of video. --> */}
+//                         <canvas></canvas>
+//                     </div>
+//                 </div>
+//                 <div className="col-md-6">
+//                     {customers[customerID].display_name}
+//                     {customers[customerID].address}
+//                     <ButtonCustomerEdit onClick={onClickEdit}/>
+//                 </div>
+//             </div>
+//         </div>
+//         }
+//     </div>
+
+
+// export default CustomerDisplay
