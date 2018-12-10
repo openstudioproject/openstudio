4164b29d0f62baae54cb9974297f5b67e2801179
Edwin van de Ven <edwin@simplycode.nl>
2018-12-10 16:06:07 +0100

    Product search reducer

--- react_frontend_src/pos/src/app/shop/products/duck/reducers.js
+++ react_frontend_src/pos/src/app/shop/products/duck/reducers.js
@@ -19,6 +19,36 @@ export const shopProductsReducer = (state = {}, action={ type: null }) => {
                 loaded: true,
                 data: action.data.data,
             }
+        case T.CLEAR_SEARCH_TIMEOUT:
+            return {
+                ...state,
+                searchTimeout: clearTimeout(state.searchTimeout), 
+            }
+        case T.SET_SEARCH_TIMEOUT:
+            return {
+                ...state,
+                searchTimeout: action.timeout,
+            }
+        case T.CLEAR_SEARCH_PRODUCT_ID:
+            return {
+                ...state,
+                searchID: null,
+            }
+        case T.SET_SEARCH_PRODUCT_ID:
+            return {
+                ...state,
+                searchID: action.id,
+            }
+        case T.CLEAR_SEARCH_VALUE:
+            return {
+                ...state,
+                search_value: "",
+            }
+        case T.SET_SEARCH_VALUE:
+            return {
+                ...state,
+                search_value: action.value.toLowerCase(),
+            }
         default:
             return {
                 ...state
