183353e851e8c95ba6421f5feaa7676a98bea7ea
Edwin van de Ven <edwin@simplycode.nl>
2018-12-12 14:14:54 +0100

    Checkin Reducer

--- react_frontend_src/pos/src/store/reducers.js
+++ react_frontend_src/pos/src/store/reducers.js
@@ -3,20 +3,22 @@ import {
   appReducer,
   localeReducer,
  } from '../app/duck/reducers'
- import  checkinAttendanceReducer from '../app/checkin/attendance/duck'
- import  checkinBookReducer from '../app/checkin/book/duck'
- import  checkinClassesReducer from '../app/checkin/classes/duck'
- import  checkinRevenueReducer from '../app/checkin/revenue/duck'
- import customersReducer from '../app/customers/reducers'
- import shopReducer from '../app/shop/reducers'
+//  import  checkinAttendanceReducer from '../app/checkin/attendance/duck'
+//  import  checkinBookReducer from '../app/checkin/book/duck'
+//  import  checkinClassesReducer from '../app/checkin/classes/duck'
+//  import  checkinRevenueReducer from '../app/checkin/revenue/duck'
+import checkinReducer from '../app/checkin/reducers'
+import customersReducer from '../app/customers/reducers'
+import shopReducer from '../app/shop/reducers'
 
 const rootReducer = combineReducers({
   app: appReducer,
   locale: localeReducer,
-  checkin_attendance: checkinAttendanceReducer,
-  checkin_book: checkinBookReducer,
-  checkin_classes: checkinClassesReducer,
-  checkin_revenue: checkinRevenueReducer,
+  checkin: checkinReducer,
+  // checkin_attendance: checkinAttendanceReducer,
+  // checkin_book: checkinBookReducer,
+  // checkin_classes: checkinClassesReducer,
+  // checkin_revenue: checkinRevenueReducer,
   customers: customersReducer,
   shop: shopReducer
   // home: homeReducer
