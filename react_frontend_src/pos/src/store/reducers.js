4c0e25ef7f6a66c2dd92447f701ec5a82135dcfa
Edwin van de Ven <edwin@simplycode.nl>
2018-12-12 14:15:30 +0100

    Code cleanup

--- react_frontend_src/pos/src/store/reducers.js
+++ react_frontend_src/pos/src/store/reducers.js
@@ -3,10 +3,7 @@ import {
   appReducer,
   localeReducer,
  } from '../app/duck/reducers'
-//  import  checkinAttendanceReducer from '../app/checkin/attendance/duck'
-//  import  checkinBookReducer from '../app/checkin/book/duck'
-//  import  checkinClassesReducer from '../app/checkin/classes/duck'
-//  import  checkinRevenueReducer from '../app/checkin/revenue/duck'
+
 import checkinReducer from '../app/checkin/reducers'
 import customersReducer from '../app/customers/reducers'
 import shopReducer from '../app/shop/reducers'
@@ -15,10 +12,6 @@ const rootReducer = combineReducers({
   app: appReducer,
   locale: localeReducer,
   checkin: checkinReducer,
-  // checkin_attendance: checkinAttendanceReducer,
-  // checkin_book: checkinBookReducer,
-  // checkin_classes: checkinClassesReducer,
-  // checkin_revenue: checkinRevenueReducer,
   customers: customersReducer,
   shop: shopReducer
   // home: homeReducer
