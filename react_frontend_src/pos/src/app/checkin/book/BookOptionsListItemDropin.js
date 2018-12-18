73185741cc0f50ecc61ce1fd0ae0a535f27d4473
Edwin van de Ven <edwin@simplycode.nl>
2018-12-18 20:25:13 +0100

    Check for drop-in & trial prices

--- react_frontend_src/pos/src/app/checkin/book/BookOptionsListItemDropin.js
+++ react_frontend_src/pos/src/app/checkin/book/BookOptionsListItemDropin.js
@@ -6,23 +6,25 @@ import Currency from "../../../components/ui/Currency"
 const BookOptionsListItemDropin = injectIntl(({data, intl, onClick=f=>f}) => 
     <div className="col-md-3"
          onClick={() => onClick(data)}>
-
-        <div className="small-box bg-aqua-active">
-            <div className="inner">
-                <h4>
-                    Drop-in class
-                </h4>
-                <h4>
-                    <Currency amount={data.Price} />
-                </h4>
-                <p>
-                    {data.Message}
-                </p>
+        {(data.Price) ?
+            <div className="small-box bg-aqua-active">
+                <div className="inner">
+                    <h4>
+                        Drop-in class
+                    </h4>
+                    <h4>
+                        <Currency amount={data.Price} />
+                    </h4>
+                    <p>
+                        {data.Message}
+                    </p>
+                </div>
+                <div className="icon">
+                <i className="fa fa-id-card-o"></i>
+                </div>
             </div>
-            <div className="icon">
-              <i className="fa fa-id-card-o"></i>
-            </div>
-         </div>
+            : "Drop-in class price not set for this class"
+        }
     </div>
 )
 
--- react_frontend_src/pos/src/app/checkin/book/BookOptionsListItemTrial.js
+++ react_frontend_src/pos/src/app/checkin/book/BookOptionsListItemTrial.js
@@ -6,23 +6,25 @@ import Currency from "../../../components/ui/Currency"
 const BookOptionsListItemTrial = injectIntl(({data, intl, onClick=f=>f}) => 
     <div className="col-md-3"
          onClick={() => onClick(data)}>
-
-        <div className="small-box bg-green">
-            <div className="inner">
-                <h4>
-                    Trial class
-                </h4>
-                <h4>
-                    <Currency amount={data.Price} />
-                </h4>
-                <p>
-                    {data.Message}
-                </p>
-            </div>
-            <div className="icon">
-              <i className="fa fa-id-card-o"></i>
-            </div>
-         </div>
+        {(data.Price) ?
+            <div className="small-box bg-green">
+                <div className="inner">
+                    <h4>
+                        Trial class
+                    </h4>
+                    <h4>
+                        <Currency amount={data.Price} />
+                    </h4>
+                    <p>
+                        {data.Message}
+                    </p>
+                </div>
+                <div className="icon">
+                <i className="fa fa-id-card-o"></i>
+                </div>
+            </div> 
+            : "Trial class price not set for this class"
+        }
     </div>
 )
 
