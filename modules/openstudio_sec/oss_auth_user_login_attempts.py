02e6992c1fab6549bdfb52f9d63a09336b4f97e9
Edwin van de Ven <edwin@simplycode.nl>
2018-12-20 16:59:17 +0100

    Fix some imports

--- modules/openstudio_sec/oss_auth_user_login_attempts.py
+++ modules/openstudio_sec/oss_auth_user_login_attempts.py
@@ -3,24 +3,26 @@
 from gluon import *
 
 class OSSAULA:
-    def update_login_attempts(self, form):
+    """
+    Class to gather functions for auth_user login attempts
+    """
+
+    def update_login_attempts(self, var=None):
         """
         Store the number of failed login attempts in cache
         :param form:
         :return:
         """
         cache = current.cache
+        request = current.globalenv['request']
 
         failed_attempts_cache_key = "auth_login_failed_attempts_%s" % request.vars.email
-        print failed_attempts_cache_key
-        print "failed attempts"
 
+        # Get current failed attempts
         failed_attempts = cache.ram(failed_attempts_cache_key, lambda: 0, time_expire=1800)
-        print failed_attempts
         cache.ram.clear(regex=failed_attempts_cache_key)
-
+        # Update failed attempts
         failed_attempts = cache.ram(failed_attempts_cache_key, lambda: failed_attempts + 1, time_expire=1800)
-        print failed_attempts
 
 
     def login_check_lockout(self, form):
@@ -30,6 +32,8 @@ class OSSAULA:
         :param form:
         :return:
         """
+        T = current.T
+        auth = current.auth
         cache = current.cache
 
         allowed_attempts = 7
