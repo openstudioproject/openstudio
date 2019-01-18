8f496e6a2df7abb55841f16ffb49fd4931a418fa
Edwin van de Ven <edwin@simplycode.nl>
2019-01-18 17:52:41 +0100

    Improve renew memberships scheduler task

--- react_frontend_src/pos/src/app/checkin/classes/ClassesListComponent.js
+++ react_frontend_src/pos/src/app/checkin/classes/ClassesListComponent.js
@@ -5,6 +5,9 @@ import ClassesListClass from "./ClassesListClassComponent"
 
 const ClassesList = ({classes}) => 
     <div className="box box-solid">
+        <div className="box-header with-border">
+            <h3 className="box-title">Please select a class</h3>
+        </div>
         <div className="box-body">
             {classes.map((cls) => 
                 <ClassesListClass key={"cls_" + v4()}
