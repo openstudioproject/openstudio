d5956f66463bcca2d98192835c4a2ee5ce71c25a
Edwin van de Ven <edwin@simplycode.nl>
2019-01-22 19:42:32 +0100

    back-end get customer classcards

--- controllers/pos.py
+++ controllers/pos.py
@@ -693,10 +693,49 @@ def get_customers_memberships():
 
 
 @auth.requires(auth.has_membership(group_id='Admins') or \
+               auth.has_permission('read', 'customers_classcards'))
+def get_customers_classcards():
+    """
+    List customer subscriptions, excluding cards that ended more then 
+    7 months ago
+    """
+    set_headers()
+
+    dont_show_after = TODAY_LOCAL - datetime.timedelta(days=217)
+    query = (db.customers_classcards.Startdate <= TODAY_LOCAL) &\
+            ((db.customers_classcards.Enddate >= dont_show_after) |\
+             (db.customers_classcards.Enddate == None))
+
+
+    rows = db(query).select(
+        db.customers_classcards.id,
+        db.customers_classcards.auth_customer_id,
+        db.customers_classcards.school_classcards_id,
+        db.customers_classcards.Startdate,
+        db.customers_classcards.Enddate,
+    )
+
+    classcards = {}
+    for i, row in enumerate(rows):
+        repr_row = list(rows[i:i + 1].render())[0]
+
+        classcards[row.auth_customer_id] = {
+            'id': row.id,
+            'auth_customer_id': row.auth_customer_id,
+            'name': repr_row.school_classcards_id,
+            'start': row.Startdate,
+            'end': row.Enddate,
+        }
+
+    return classcards
+
+
+@auth.requires(auth.has_membership(group_id='Admins') or \
                auth.has_permission('read', 'customers_subscriptions'))
 def get_customers_subscriptions():
     """
-    List customer subscriptions, from the last 400 days
+    List customer subscriptions, excluding subscriptions that ended more then 
+    7 months ago
     """
     set_headers()
 
