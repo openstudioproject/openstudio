892be981018a9d93935929d34bfd96d61c22206f
Edwin van de Ven <edwin@simplycode.nl>
2019-02-04 13:52:48 +0100

    Delete reminders

--- modules/openstudio/os_sys_email_reminders.py
+++ modules/openstudio/os_sys_email_reminders.py
@@ -27,12 +27,19 @@ class SysEmailReminders:
 
         :return: HTML table of reminders
         """
+        from os_gui import OsGui
+
         T = current.T
+        auth = current.auth
+        os_gui = OsGui()
 
         rows = self.list()
 
         if not len(rows):
             return T("No reminders configured")
+
+        permission = auth.has_membership(group_id='Admins') or \
+                     auth.has_permission('delete', 'sys_email_reminders')
 
 
         header = THEAD(TR(
@@ -44,9 +51,31 @@ class SysEmailReminders:
         for row in rows:
             table.append(TR(
                 TD(T("Send email %s days before class date" % row.Days)),
-                TD()
+                TD(self._list_formatted_get_delete_button(row.id, permission, os_gui))
             ))
 
         return table
 
 
+    def _list_formatted_get_delete_button(self, serID, permission, os_gui):
+        """
+        HTML delete button
+        :param serID: db.sys_email_reminders.id
+        :param permission: Bool
+        :return: HTML delete button
+        """
+        if not permission:
+            return ''
+
+        T = current.T
+
+        onclick = "return confirm('" + \
+             T("Are you sure you want to delete this reminder?") + ' ' + \
+             "');"
+
+        return os_gui.get_button(
+            'delete_notext',
+            URL('reminder_delete', vars={'serID': serID}),
+            onclick=onclick,
+            _class='pull-right'
+        )
