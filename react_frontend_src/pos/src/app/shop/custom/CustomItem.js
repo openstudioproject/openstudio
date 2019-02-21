a01b0862a7be7ef9393fa960aa8a2f0210804548
Edwin van de Ven <edwin@simplycode.nl>
2019-02-21 16:18:13 +0100

    Basic Formik form for custom item

--- react_frontend_src/pos/src/app/shop/custom/CustomItem.js
+++ react_frontend_src/pos/src/app/shop/custom/CustomItem.js
@@ -3,6 +3,7 @@ import { intlShape } from "react-intl"
 import PropTypes from "prop-types"
 import { v4 } from "uuid"
 import validator from 'validator'
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 import ShopTemplate from '../ShopTemplate'
 
@@ -34,7 +35,59 @@ class CustomItem extends Component {
             <ShopTemplate app_state={this.props.app}>
                 { this.props.loaded ? 
                     <div>
-                        Hello world!
+                        <Formik
+                            initialValues={{ product: '', description: '', price: 0, quantity: 1 }}
+                            validate={values => {
+                                let errors = {};
+                                if (!values.product) {
+                                    errors.product = 'Required';
+                                } 
+                                // else if (
+                                // !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                                // ) {
+                                // errors.email = 'Invalid email address';
+                                // }
+                                if (!values.description) {
+                                    errors.description = 'Required'
+                                }
+
+                                return errors;
+                            }}
+                            onSubmit={(values, { setSubmitting }) => {
+                                setTimeout(() => {
+                                alert(JSON.stringify(values, null, 2));
+                                setSubmitting(false);
+                                }, 400);
+                            }}
+                            >
+                            {({ isSubmitting }) => (
+                                <Form>
+                                    <div className="form-group">
+                                        <label>Product</label>
+                                        <Field className="form-control" type="text" name="product" autoComplete="off" />
+                                        <ErrorMessage name="product" component="div" />
+                                    </div>
+                                    <div className="form-group">
+                                        <label>Description</label>
+                                        <Field className="form-control" type="text" name="description" autoComplete="off" />
+                                        <ErrorMessage name="description" component="div" />
+                                    </div>
+                                    <div className="form-group">
+                                        <label>Price</label>
+                                        <Field className="form-control" type="text" name="price" autoComplete="off" />
+                                        <ErrorMessage name="price" component="div" />
+                                    </div>
+                                    <div className="form-group">
+                                        <label>Quantity</label>
+                                        <Field className="form-control" type="number" name="quantity" autoComplete="off" />
+                                        <ErrorMessage name="quantity" component="div" />
+                                    </div>
+                                    <button className="btn btn-primary" type="submit" disabled={isSubmitting}>
+                                        Add to cart
+                                    </button>
+                                </Form>
+                            )}
+                        </Formik>
                     </div> :
                      "Loading..."
                 }
