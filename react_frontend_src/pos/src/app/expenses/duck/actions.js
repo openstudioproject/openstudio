42aacda1f49ac9f323bfd3ffc086a7aece662755
Edwin van de Ven <edwin@simplycode.nl>
2019-03-25 14:56:15 +0900

    Request & receive expenses actions

--- react_frontend_src/pos/src/app/expenses/duck/actions.js
+++ react_frontend_src/pos/src/app/expenses/duck/actions.js
@@ -1,147 +1,25 @@
 import T from './types'
 
 
-export const requestCustomers = () =>
+export const requestExpensess = () =>
     ({
-        type: T.REQUEST_CUSTOMERS
+        type: T.REQUEST_EXPENSES
     })
 
-export const receiveCustomers = (data) =>
+export const receiveExpenses = (data) =>
     ({
-        type: T.RECEIVE_CUSTOMERS,
+        type: T.RECEIVE_EXPENSES,
         data
     })
 
-export const requestCreateCustomer = (data) =>
-    ({
-        type: T.REQUEST_CREATE_CUSTOMER,
-        data
-    })
+// export const requestCreateCustomer = (data) =>
+//     ({
+//         type: T.REQUEST_CREATE_CUSTOMER,
+//         data
+//     })
 
-export const receiveCreateCustomer = (data) =>
-    ({
-        type: T.RECEIVE_CREATE_CUSTOMER,
-        data
-    })
-
-export const clearCreateCustomerErrorData = () =>
-    ({
-        type: T.CLEAR_CREATE_CUSTOMER_ERROR_DATA
-    })
-
-export const requestUpdateCustomer = (data) =>
-    ({
-        type: T.REQUEST_UPDATE_CUSTOMER,
-        data
-    })
-
-export const receiveUpdateCustomer = (data) =>
-    ({
-        type: T.RECEIVE_UPDATE_CUSTOMER,
-        data
-    })
-
-export const clearUpdateCustomerErrorData = () =>
-    ({
-        type: T.CLEAR_UPDATE_CUSTOMER_ERROR_DATA
-    })
-
-export const requestSaveCameraAppSnap = () =>
-    ({
-        type: T.REQUEST_SAVE_CAMERA_APP_SNAP
-    })
-
-export const receiveSaveCameraAppSnap = (data) =>
-    ({
-        type: T.RECEIVE_SAVE_CAMERA_APP_SNAP,
-        data
-    })
-
-export const clearSearchTimeout = () =>
-    ({
-        type: T.CLEAR_SEARCH_TIMEOUT
-    })
-
-export const setSearchTimeout = (timeout) =>
-    ({
-        type: T.SET_SEARCH_TIMEOUT,
-        timeout,
-    })
-
-export const clearDisplayCustomerID = () =>
-    ({
-        type: T.CLEAR_DISPLAY_CUSTOMER_ID
-    })
-
-export const setDisplayCustomerID = (id) =>
-    ({
-        type: T.SET_DISPLAY_CUSTOMER_ID,
-        id
-    })
-
-export const clearSearchCustomerID = () =>
-    ({
-        type: T.CLEAR_SEARCH_CUSTOMER_ID
-    })
-
-export const setSearchCustomerID = (id) =>
-    ({
-        type: T.SET_SEARCH_CUSTOMER_ID,
-        id
-    })
-
-export const clearSearchValue = () =>
-    ({
-        type: T.CLEAR_SEARCH_VALUE
-    })
-
-export const setSearchValue = (value) =>
-    ({
-        type: T.SET_SEARCH_VALUE,
-        value
-    })
-
-export const clearSelectedCustomerID = () =>
-    ({
-        type: T.CLEAR_SELECTED_CUSTOMER_ID
-    })
-
-export const setSelectedCustomerID = (id) =>
-    ({
-        type: T.SET_SELECTED_CUSTOMER_ID,
-        id
-    })
-
-export const setCreateCustomerStatus = (status) =>
-    ({
-        type: T.SET_CREATE_CUSTOMER_STATUS,
-        status
-    })
-
-export const setUpdateCustomerStatus = (status) =>
-    ({
-        type: T.SET_UPDATE_CUSTOMER_STATUS,
-        status
-    })
-
-export const setRedirectNextComponent = (component) =>
-    ({
-        type: T.SET_REDIRECT_NEXT_COMPONENT,
-        component
-    })
-
-export const clearRedirectNextComponent = () =>
-    ({
-        type: T.CLEAR_REDIRECT_NEXT_COMPONENT,
-    })
-
-export const setCameraAppSnap = (data) =>
-    ({
-        type: T.SET_CAMERA_APP_SNAP,
-        data
-    })
-
-export const clearCameraAppSnap = () =>
-    ({
-        type: T.CLEAR_CAMERA_APP_SNAP,
-    })
+// export const receiveCreateCustomer = (data) =>
+//     ({
+//         type: T.RECEIVE_CREATE_CUSTOMER,
+//         data
+//     })
