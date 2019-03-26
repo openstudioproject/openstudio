c4a28be38ba219e1781e956b14e18a4f6333ddc9
Edwin van de Ven <edwin@simplycode.nl>
2019-03-27 02:16:50 +0900

    Expense input same style as customer create

--- react_frontend_src/pos/src/app/expenses/ExpenseAdd.js
+++ react_frontend_src/pos/src/app/expenses/ExpenseAdd.js
@@ -6,7 +6,7 @@ import { NavLink } from 'react-router-dom'
 import { Formik, Form, Field, ErrorMessage } from 'formik'
 
 import ExpensesTemplate from "./ExpensesTemplateContainer"
-import EXPENSE_SCHEMA from "./yupSchema"
+import FormError from "./FormError"
 
 class ExpenseAdd extends Component {
     constructor(props) {
@@ -22,118 +22,151 @@ class ExpenseAdd extends Component {
 
     render() {
         const history = this.props.history
+        const error_data = this.props.error_data || {}
         const return_url = '/expenses'
 
         return (
             <ExpensesTemplate app={this.props.app} expenses={this.props.expenses} setPageTitle={this.props.setPageTitle}>
-                <Formik
-                    initialValues={{ name: '', code: '' }}
-                    validationSchema={EXPENSE_SCHEMA}
-                    onSubmit={(values, { setSubmitting }) => {
-                        // dispatch actions here
-
-
-                        // addLocation({ variables: {
-                        //     input: {
-                        //     name: values.name, 
-                        //     code: values.code
-                        //     }
-                        // }, refetchQueries: [
-                        //     {query: GET_COSTCENTERS_QUERY, variables: {"archived": false }}
-                        // ]})
-                        // .then(({ data }) => {
-                        //     console.log('got data', data);
-                        //     toast.success((t('finance.costcenters.toast_add_success')), {
-                        //         position: toast.POSITION.BOTTOM_RIGHT
-                        //         })
-                        //     }).catch((error) => {
-                        //     toast.error((t('toast_server_error')) + ': ' +  error, {
-                        //         position: toast.POSITION.BOTTOM_RIGHT
-                        //         })
-                        //     console.log('there was an error sending the query', error)
-                        //     setSubmitting(false)
-                        //     })
-                    }}
-                    >
-                    {({ isSubmitting, errors }) => (
-                        <div className="box box-solid">
-                            <div className='box-header'>
-                                <h3 className="box-title">Add expense</h3>
-                            </div>
-                            <Form>
-                                <div className="box-body">
-                                    <label>Amount</label>
-                                    <Field type="text" 
-                                        name="amount" 
-                                        className={(errors.amount) ? "form-control is-invalid" : "form-control"} 
-                                        autoComplete="off" />
-                                    <ErrorMessage name="amount" component="span" className="invalid-feedback" />
-                                    <label>Tax rate</label>
-                                    <Field component="select" 
-                                           name="tax_rates_id" 
-                                           className={(errors.tax_rates_id) ? "form-control is-invalid" : "form-control"} 
-                                           autoComplete="off">
-                                        {/* <option value="IN">{t('finance.taxrates.including')}</option>
-                                        <option value="EX">{t('finance.taxrates.excluding')}</option> */}
-                                    </Field>
-                                    <label>Reference</label>
-                                    <Field type="text" 
-                                           name="your_reference" 
-                                           className={(errors.your_reference) ? "form-control is-invalid" : "form-control"} 
-                                           autoComplete="off" />
-                                    <ErrorMessage name="your_reference" component="span" className="invalid-feedback" />
-                                    <label>Description</label>
-                                    <Field type="text" 
-                                           name="description" 
-                                           className={(errors.description) ? "form-control is-invalid" : "form-control"} 
-                                           autoComplete="off" />
-                                    <ErrorMessage name="description" component="span" className="invalid-feedback" />
-                                    <label>Note</label>
-                                    <Field type="textarea" 
-                                           name="note" 
-                                           className={(errors.note) ? "form-control is-invalid" : "form-control"} 
-                                           autoComplete="off" />
-                                    <ErrorMessage name="note" component="span" className="invalid-feedback" />
-                                    {/* <Form.Group label={t('finance.costcenters.code')}>
-                                        <Field type="text" 
-                                                name="code" 
-                                                className={(errors.code) ? "form-control is-invalid" : "form-control"} 
-                                                autoComplete="off" />
-                                        <ErrorMessage name="code" component="span" className="invalid-feedback" />
-                                    </Form.Group> */}
-                                </div>
-                                <div className="box-footer">
-                                    <button className="btn btn-primary"
-                                            type="submit"
-                                            disabled={isSubmitting}
-                                            >
-                                        Save
-                                    </button>
-                                    <button className="btn btn-link" 
-                                            type="button"
-                                            onClick={() => history.push(return_url)}
-                                            disabled={isSubmitting}>
-                                        Cancel
-                                    </button>
-                                    {/* <Button 
-                                        color="primary"
-                                        className="pull-right" 
-                                        type="submit" 
-                                        disabled={isSubmitting}
-                                    >
-                                        {t('submit')}
-                                    </Button>
-                                    <Button color="link" onClick={() => history.push(return_url)}>
-                                        {t('cancel')}
-                                    </Button> */}
-                                </div>
-                            </Form>
+                <div className="box box-solid">
+                    <div className='box-header'>
+                        <h3 className="box-title">Add expense</h3>
+                    </div>
+                    <form onSubmit={() => this.props.onSubmit}>
+                        <div className="box-body">
+                            <label htmlFor="amount">Amount</label>
+                            <input 
+                                id="amount" 
+                                className="form-control"
+                                name="amount" 
+                                type="text" 
+                            />
+                            <FormError message={ (error_data.amount) ? error_data.amount : "" } />
                         </div>
-                    )}
-                </Formik>
+                        <div className="box-footer">
+                            <button className="btn btn-primary"
+                                    type="submit">
+                                Save
+                            </button>
+                            <button className="btn btn-link"
+                                    type="button"
+                                    onClick={() => history.push(return_url)}>
+                                Cancel
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </ExpensesTemplate>
         )
     }
 }
 
 export default withRouter(ExpenseAdd)
+
+
+
+// import React, { Component } from "react"
+// import { intlShape } from "react-intl"
+// import PropTypes from "prop-types"
+// import validator from 'validator'
+// import { v4 } from "uuid"
+// import Inputmask from "inputmask"
+// // import Inputmask from "inputmask/dist/inputmask/inputmask.date.extensions";
+
+// import CustomerFormError from "./CustomerFormError"
+
+
+
+
+// class CustomerFormCreate extends Component {
+//     constructor(props) {
+//         super(props)
+//         console.log(props)
+//         this.inputDateOfBirth = React.createRef();
+//     }
+
+//     PropTypes = {
+//         intl: intlShape.isRequired,
+//     }
+
+//     componentWillMount() {
+//     }
+
+//     componentDidMount() {
+//         Inputmask({"placeholder": this.props.inputmask_date}).mask(this.inputDateOfBirth.current)
+//         // this.inputDateOfBirth.current.inputmask(this.props.inputmask_date, { 'placeholder': this.props.inputmask_date })
+//     }
+
+
+//     render() {
+//         // const inputmask_date = this.props.inputmask_date
+//         const inputmask_date = this.props.inputmask_date
+//         const error_data = this.props.error_data
+//         const onSubmit = this.props.onSubmit
+//         const onCancel = this.props.onCancel        
+
+//         return (
+//             <div className="box box-solid"> 
+//                 <div className="box-header">
+//                     <h3 className="box-title">Add customer</h3>
+//                     <button className="btn btn-default pull-right"
+//                             onClick={onCancel}>
+//                         Cancel
+//                     </button>
+//                 </div>
+//                 <div className="box-body">
+//                     <form onSubmit={onSubmit}>
+//                         <label htmlFor="first_name">First Name</label>
+//                         <input 
+//                             id="first_name" 
+//                             className="form-control"
+//                             name="first_name" 
+//                             type="text" 
+//                         />
+//                         <CustomerFormError message={ (error_data.first_name) ? error_data.first_name : "" } />
+//                         <label htmlFor="last_name">Last Name</label>
+//                         <input 
+//                             id="last_name" 
+//                             className="form-control"
+//                             name="last_name" 
+//                             type="text" 
+//                         />
+//                         <CustomerFormError message={ (error_data.last_name) ? error_data.last_name : "" } />
+//                         <label htmlFor="email">Email</label>
+//                         <input 
+//                             id="email" 
+//                             className="form-control"
+//                             name="email" 
+//                             type="text" 
+//                         />
+//                         <CustomerFormError message={ (error_data.email) ? error_data.email : "" } />
+//                         <label htmlFor="email">Mobile</label>
+//                         <input 
+//                             id="mobile" 
+//                             className="form-control"
+//                             name="mobile" 
+//                             type="text" 
+//                         />
+//                         <CustomerFormError message={ (error_data.mobile) ? error_data.mobile : "" } />
+//                         <label htmlFor="email">Date of birth</label>
+//                         <input 
+//                             id="date_of_birth" 
+//                             className="form-control date-inputmask"
+//                             autoComplete="off"
+//                             name="date_of_birth" 
+//                             type="text" 
+//                             data-inputmask={"'alias': 'datetime', 'inputFormat': '" + inputmask_date + "'"}
+//                             data-mask="true"
+//                             ref={this.inputDateOfBirth}
+//                         />
+//                         <CustomerFormError message={ (error_data.date_of_birth) ? error_data.date_of_birth : "" } />
+//                         <br />
+//                         <button className="btn btn-primary">Save</button>
+//                     </form>
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
+
+// export default CustomerFormCreate
+
---
+++ react_frontend_src/pos/src/app/expenses/FormError.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const FormError = ({message}) => 
+    <div className="error"> 
+        { message }
+    </div>
+
+
+export default FormError
