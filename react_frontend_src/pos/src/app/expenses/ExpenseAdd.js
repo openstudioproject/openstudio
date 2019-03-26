89e0b93164a015aab7e733b7b0fdd46467704df5
Edwin van de Ven <edwin@simplycode.nl>
2019-03-26 18:43:06 +0900

    Add Expense Add component

--- react_frontend_src/pos/src/app/expenses/ExpenseAdd.js
+++ react_frontend_src/pos/src/app/expenses/ExpenseAdd.js
@@ -3,31 +3,29 @@ import { intlShape } from "react-intl"
 import PropTypes from "prop-types"
 import { NavLink } from 'react-router-dom'
 
-import PageTemplate from "../../components/PageTemplate"
 import ExpensesTemplate from "./ExpensesTemplate"
 
 
-class Expenses extends Component {
+class ExpenseAdd extends Component {
     constructor(props) {
         super(props)
-        console.log("Expenses props:")
+        console.log("Expense Add props:")
         console.log(props)
     }
 
     PropTypes = {
         intl: intlShape.isRequired,
         app: PropTypes.object,
-        expenses: PropTypes.object
     }
 
     render() {
 
         return (
             <ExpensesTemplate app={this.props.app} expenses={this.props.expenses} setPageTitle={this.props.setPageTitle}>
-                Select an expense from the list to edit it and click the add button to add a new one.
+                Form here
             </ExpensesTemplate>
         )
     }
 }
 
-export default Expenses
+export default ExpensesAdd
