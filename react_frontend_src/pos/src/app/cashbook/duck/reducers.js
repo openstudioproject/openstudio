a8163e4783c989b1deb4f45187f42d52aea32d90
Edwin van de Ven <edwin@simplycode.nl>
2019-04-02 15:13:19 +0900

    Delete reducers

--- react_frontend_src/pos/src/app/cashbook/duck/reducers.js
+++ react_frontend_src/pos/src/app/cashbook/duck/reducers.js
@@ -59,6 +59,24 @@ export const cashbookReducer = (state = {}, action={ type: null }) => {
                 ...state,
                 expenses_selectedID: null
             }
+        case T.REQUEST_CREATE_EXPENSE:
+            return {
+                ...state,
+                expense_create: true,
+            }
+        case T.RECEIVE_CREATE_EXPENSE:
+            let return_value = {
+                ...state,
+                expense_create: false,
+                expense_create_error_data: action.data.result.errors,
+            }
+            
+            if (action.data.error == false) {
+                return_value['expenses_data'] = {
+                    ...state.expenses_data,
+                    [action.data.result.id]: action.data.expense_data
+               }
+            }
         case T.REQUEST_UPDATE_EXPENSE:
             return {
                 ...state,
@@ -82,23 +100,24 @@ export const cashbookReducer = (state = {}, action={ type: null }) => {
                     expense_create_error_data: action.data.result.errors
                 }
             }
-        case T.REQUEST_CREATE_EXPENSE:
+        case T.REQUEST_DELETE_EXPENSE:
             return {
                 ...state,
-                expense_create: true,
+                expense_delete: true
             }
-        case T.RECEIVE_CREATE_EXPENSE:
-            let return_value = {
+        case T.RECEIVE_DELETE_EXPENSE:
+            let filtered_expenses_data
+            if (action.data.error == true) {
+                filtered_expenses_data = state.expenses_data
+            } else {
+                filtered_expenses_data = state.expense_data
+                delete filtered_expenses_data[action.data.id]
+            }
+
+            return {
                 ...state,
-                expense_create: false,
-                expense_create_error_data: action.data.result.errors,
-            }
-            
-            if (action.data.error == false) {
-                return_value['expenses_data'] = {
-                    ...state.expenses_data,
-                    [action.data.result.id]: action.data.expense_data
-               }
+                expense_delete: false,
+                expense_data: filtered_expenses_data
             }
         default:
             return {
