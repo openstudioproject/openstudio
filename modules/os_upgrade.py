1834df188123216061bdffce07509ea5aa738d2c
Edwin van de Ven <edwin@simplycode.nl>
2019-04-05 15:01:00 +0900

    Bump version

--- modules/os_upgrade.py
+++ modules/os_upgrade.py
@@ -19,7 +19,7 @@ def set_version():
 
     # set release
     row = db.sys_properties(Property='VersionRelease')
-    release = '3'
+    release = '4'
     if not row:
         db.sys_properties.insert(Property='VersionRelease',
                                  PropertyValue=release)
