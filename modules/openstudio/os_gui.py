6b7b4647cb0bb5477839d166310b77db1f513d91
Edwin van de Ven <edwin@simplycode.nl>
2019-05-03 13:03:53 +0900

    Fix utf-8 compatability for modal trigger buttons

--- modules/openstudio/os_gui.py
+++ modules/openstudio/os_gui.py
@@ -223,7 +223,11 @@ class OsGui:
         if button_title:
             button_xml += ' title="' + button_title + '" '
         button_xml += 'data-toggle="modal" data-target=".' + modal_class + '"> '
-        button_xml += button_text.decode('utf-8')
+        try:
+            button_xml += button_text.decode('utf-8')
+        except AttributeError:
+            # button_xml might be XML() or other object that doesn't have a decode method
+            button_xml += button_text
         button_xml += '</button>'
         return XML(button_xml)
 
