262e99a9e0c0bc974856d1fa5c469e9c25d3ef7d
Edwin van de Ven <edwin@simplycode.nl>
2019-05-21 19:35:02 +0900

    Remove unused files

--- react_frontend_src/pos/src/app/customers/notes/duck/index.js
+++
@@ -1,5 +0,0 @@
-// index.js
-import { listReducer } from './reducers'
-export { default as customersListOperations } from './operations'
-export { default as customersTypes } from './types'
-export default listReducer
