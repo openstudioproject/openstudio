388c74f694d5b75a0682f6a0f8490ed45aee876a
Edwin van de Ven <edwin@simplycode.nl>
2019-06-21 15:54:30 +0200

    Code cleanup

--- controllers/reports_tax_summary.py
+++ controllers/reports_tax_summary.py
@@ -1,31 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# from general_helpers import datestr_to_python
-# from general_helpers import get_label
-# from general_helpers import get_submenu
-# from general_helpers import get_months_list
-# from general_helpers import get_last_day_month
-# from general_helpers import get_classname
-# from general_helpers import User_helpers
-# from general_helpers import Memo_links
-# from general_helpers import class_get_teachers
-# from general_helpers import max_string_length
-# from general_helpers import iso_to_gregorian
-# from general_helpers import classes_get_status
-# from general_helpers import set_form_id_and_get_submit_button
-#
-# from gluon.tools import prettydate
-#
-# from openstudio.os_classcards_helper import ClasscardsHelper
-# from openstudio.os_class import Class
-# from openstudio.os_class_schedule import ClassSchedule
-# from openstudio.os_attendance_helper import AttendanceHelper
-# from openstudio.os_reports import Reports
-# from openstudio.os_invoice import Invoice
-# from openstudio.os_invoices import Invoices
-# from openstudio.os_school_subscription import SchoolSubscription
-# from openstudio.os_customer_classcard import CustomerClasscard
-#
 import datetime
 # import operator
 import cStringIO
