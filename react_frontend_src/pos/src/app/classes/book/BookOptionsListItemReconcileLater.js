64130ea14350bfef4c01be110067acf25a3174f8
Edwin van de Ven <edwin@simplycode.nl>
2019-07-05 09:45:55 +0200

    Add ReconcileLater check-in component

---
+++ react_frontend_src/pos/src/app/classes/book/BookOptionsListItemReconcileLater.js
@@ -0,0 +1,28 @@
+import React from "react"
+import { injectIntl } from 'react-intl'
+
+import Currency from "../../../components/ui/Currency"
+
+const BookOptionsListItemReconcileLater = injectIntl(({data, intl, onClick=f=>f}) => 
+    <div className="col-md-3"
+         onClick={() => onClick(data)}>
+        <div className="small-box bg-green">
+            <div className="inner">
+                <h4>
+                    Reconcile later
+                </h4>
+                <h4>
+                    Pay later
+                </h4>
+                <p>
+                    Use this option to check-in a drop-in class that will be paid later
+                </p>
+            </div>
+            <div className="icon">
+            <i className="fa fa-user-o"></i>
+            </div>
+        </div> 
+    </div>
+)
+
+export default BookOptionsListItemReconcileLater
