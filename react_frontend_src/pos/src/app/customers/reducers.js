3395cdae0764e635960f898f40ac71bc0ad5fb7d
Edwin van de Ven <edwin@simplycode.nl>
2019-07-08 11:25:37 +0200

    Add schoolInfo reducer to customer reducer

--- react_frontend_src/pos/src/app/customers/reducers.js
+++ react_frontend_src/pos/src/app/customers/reducers.js
@@ -4,13 +4,15 @@ import customersClasscardsReducer from './classcards/duck'
 import customersSubscriptionsReducer from './subscriptions/duck'
 import customersMembershipsReducer from './memberships/duck'
 import customersMembershipsTodayReducer from './memberships_today/duck'
+import customersSchoolInfoReducer from './school_info/duck'
 
 const customersReducer = combineReducers({
   list: listReducer,
   classcards: customersClasscardsReducer,
   subscriptions: customersSubscriptionsReducer,
   memberships: customersMembershipsReducer,
-  memberships_today: customersMembershipsTodayReducer
+  memberships_today: customersMembershipsTodayReducer,
+  school_info: customersSchoolInfoReducer
 })
 
 export default customersReducer
