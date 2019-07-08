7b1980527db3741cd3e33bcd977dcdc27e281442
Edwin van de Ven <edwin@simplycode.nl>
2019-07-08 11:20:18 +0200

    School Info reducer

--- react_frontend_src/pos/src/app/customers/school_info/duck/reducers.js
+++ react_frontend_src/pos/src/app/customers/school_info/duck/reducers.js
@@ -1,20 +1,20 @@
 import T from './types'
 
-export const customersClasscardsReducer = (state = {}, action={ type: null }) => {
+export const customersSchoolInfoReducer = (state = {}, action={ type: null }) => {
     switch (action.type) {
-        case T.REQUEST_CUSTOMER_CLASSCARDS:
+        case T.REQUEST_CUSTOMER_SCHOOL_INFO:
             return {
                 ...state,
                 loading: true,
                 loaded: false,
                 loading: action.loading,
             }
-        case T.RECEIVE_CUSTOMER_CLASSCARDS:
+        case T.RECEIVE_CUSTOMER_SCHOOL_INFO:
             return {
                 ...state,
                 loading: false,
                 loaded: true,
-                data: action.data.data
+                data: action.data
             }
         default:
             return {
