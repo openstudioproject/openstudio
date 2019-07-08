1a56ca97fe7c1add63291dfb2afcc5a0a1589b5a
Edwin van de Ven <edwin@simplycode.nl>
2019-07-08 12:03:11 +0200

    Add ReconcileLaterClasses

---
+++ react_frontend_src/pos/src/app/customers/list/CustomerDisplayReconcileLaterClasses.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react"
+import { intlShape } from "react-intl"
+import PropTypes from "prop-types"
+import validator from 'validator'
+import { v4 } from "uuid"
+
+
+
+class ReconcileLaterClasses extends Component {
+    constructor(props) {
+        super(props)
+        console.log(props)
+    }
+
+    PropTypes = {
+        intl: intlShape.isRequired,
+    }
+
+    componentWillMount() {
+    }
+
+    componentDidMount() {
+    }
+
+
+    render() {
+        const data = this.props.data
+        console.log('CDRC here')
+        console.log(data)
+        const classes = data.data.reconcile_later_classes
+
+        return (
+           <div>
+                { (data.loaded) ?
+                    (classes.length) ?
+                        <div>     
+                            <b>Classes to be paid</b>
+                            {classes.map((cls, i) => 
+                                <div key={v4()}>
+                                    { cls.class_date } {cls.time_start} <br />
+                                    <span className="text-muted">
+                                        { cls.school_location } { cls.school_classtype }
+                                    </span>
+                                </div>
+                            )}
+                        </div> : '' 
+                : "Loading classes to be reconciled, please wait..."
+               }
+           </div>
+        )
+    }
+}
+
+export default ReconcileLaterClasses
+
