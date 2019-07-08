43f02b37dc8e6c6b16f74ee97a1e01e5bf7afee1
Edwin van de Ven <edwin@simplycode.nl>
2019-07-08 11:33:04 +0200

    Clear school info reducer

--- react_frontend_src/pos/src/app/customers/school_info/duck/reducers.js
+++ react_frontend_src/pos/src/app/customers/school_info/duck/reducers.js
@@ -2,12 +2,19 @@ import T from './types'
 
 export const customersSchoolInfoReducer = (state = {}, action={ type: null }) => {
     switch (action.type) {
+        case T.CLEAR_CUSTOMER_SCHOOL_INFO:
+            return {
+                ...state,
+                loading: false,
+                loaded: false,
+                data: {}
+            }
         case T.REQUEST_CUSTOMER_SCHOOL_INFO:
             return {
                 ...state,
                 loading: true,
                 loaded: false,
-                loading: action.loading,
+                data: {}
             }
         case T.RECEIVE_CUSTOMER_SCHOOL_INFO:
             return {
