81501f4b954b405eb418d6c705b5b0cfe2a6555b
Edwin van de Ven <edwin@simplycode.nl>
2019-10-31 19:46:37 +0100

    Only show not allowed messages for current subscriptions

--- react_frontend_src/pos/src/app/classes/book/BookOptionsListItemSubscription.js
+++ react_frontend_src/pos/src/app/classes/book/BookOptionsListItemSubscription.js
@@ -4,6 +4,17 @@ import { injectIntl } from 'react-intl'
 
 import customerHasRequiredMembership from './customerHasRequiredMembership'
 
+
+function getIconClass(data) {
+    if (data.Type == "subscription_shop") {
+        return "fa fa-shopping-cart"
+    } else {
+        return "fa fa-pencil-square-o"
+    }
+}
+
+
+
 const BookOptionsListItemSubscription = injectIntl(({data, customer_memberships, intl, onClick=f=>f}) => 
     <div className="col-md-3"
          onClick={(data.Allowed) ? () => onClick(data): f=>f}>
@@ -11,18 +22,19 @@ const BookOptionsListItemSubscription = injectIntl(({data, customer_memberships,
          {console.log('subscription list item')}
          {console.log(data)}
          {console.log(customer_memberships)}
+
+
          <div className={(data.Allowed) ? "small-box bg-purple" : "small-box bg-gray-active"}>
             <div className="inner">
                 <h4>
                     {data.Name}
                 </h4>
                 <p>
-                    {/* {(data.Unlimited) ? "Unlimited" : data.Credits.toFixed(1) + " credits remaining"} */}
                     {(data.Unlimited) ? "Unlimited" : data.Credits + " credits remaining"}
                 </p>
             </div>
             <div className="icon">
-              <i className="fa fa-pencil-square-o"></i>
+              <i className={getIconClass(data)}></i>
             </div>
             { (data.school_memberships_id) ? 
                 customerHasRequiredMembership(data.school_memberships_id, customer_memberships) ? 
@@ -30,7 +42,7 @@ const BookOptionsListItemSubscription = injectIntl(({data, customer_memberships,
                     : ''
             }
 
-            { !(data.Allowed) ?
+            { (!(data.Allowed) && (data.Type === "subscription")) ?
                 <span className="small-box-footer"><i className="fa fa-exclamation-circle"></i> Not allowed for this class</span>
                 : '' 
             }
