3925ebb795bca80704e88177497eb63ca63763a7
Edwin van de Ven <edwin@simplycode.nl>
2019-11-22 21:02:41 +0100

    List memberships of the past 10 years

--- controllers/pos.py
+++ controllers/pos.py
@@ -907,7 +907,7 @@ def get_customer_memberships():
 
     cuID = request.vars['id']
 
-    date_from = TODAY_LOCAL - datetime.timedelta(days=400)
+    date_from = TODAY_LOCAL - datetime.timedelta(days=3660)
 
     query = (db.customers_memberships.Startdate >= date_from) & \
             (db.customers_memberships.auth_customer_id == cuID)
