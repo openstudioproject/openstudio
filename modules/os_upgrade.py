6af8caa5a2288b20ecbd45437c8672b237853a68
Edwin van de Ven <edwin@simplycode.nl>
2020-01-17 18:17:47 +0100

    Version bump

--- modules/os_upgrade.py
+++ modules/os_upgrade.py
@@ -10,7 +10,7 @@ def set_version():
     cache_clear_sys_properties = current.globalenv['cache_clear_sys_properties']
 
     row = db.sys_properties(Property='Version')
-    version = '2019.14'
+    version = '2020.01'
     if not row:
         db.sys_properties.insert(Property='Version', PropertyValue=version)
     else:
