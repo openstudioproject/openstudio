2889c18027b838773b7b18edfe572374a7511280
Edwin van de Ven <edwin@simplycode.nl>
2020-10-11 18:53:07 +0200

    Add fn to check if a customer has a required membership on a given date
    For a classcard

--- modules/openstudio/os_customer_classcard.py
+++ modules/openstudio/os_customer_classcard.py
@@ -384,4 +384,29 @@ class CustomerClasscard:
             return permissions
         else:
             return self._get_class_permissions_format(permissions)
-        
+
+
+    def customer_has_required_membership_on_date(self, date):
+        """
+        Check if the customer this card is linked to has the required membership to be able to check-in to classes
+        :param date: datetime.date - date on which to check for the required classcard
+        :return: Boolean; True if the customer has the required membership or no membership is defined. Otherwise false.
+        """
+        from .os_customer import Customer
+
+        if not self.school_classcard.school_memberships_id:
+            return True
+
+        customer = Customer(self.classcard.auth_customer_id)
+
+        memberships = customer.get_memberships_on_date(date)
+        if not memberships:
+            return False
+
+        return_value = False
+        for membership in memberships:
+            if membership.id == self.school_classcard.auth_customer_id:
+                return_value = True
+                break
+
+        return return_value
