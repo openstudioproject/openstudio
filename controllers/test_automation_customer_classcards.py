206ae803f78ef051f52786e46a85066982145842
Edwin van de Ven <edwin@simplycode.nl>
2021-01-08 13:59:08 +0100

    Add function sysadmins can use to fix bad card extensions

--- controllers/test_automation_customer_classcards.py
+++ controllers/test_automation_customer_classcards.py
@@ -20,3 +20,41 @@ def test_extend_validity():
     days_to_add = request.vars['days_to_add']
 
     return ost.customers_classcards_extend_validity(valid_on, days_to_add)
+
+
+@auth.requires(auth.user.id == 1)
+def test_fix_extend_validity_with_bad_query():
+    """
+    Function to correct extended validity
+    :return:
+    """
+    from general_helpers import datestr_to_python
+
+    valid_on = request.vars['valid_on']
+    days_to_subtract = request.vars['days_to_subtract']
+
+    db = current.db
+    DATE_FORMAT = current.DATE_FORMAT
+    # convert input string to date obj
+    valid_on = datestr_to_python(DATE_FORMAT, valid_on)
+
+    left = [
+        db.school_classcards.on(db.customers_classcards.school_classcards_id ==
+                                db.school_classcards.id)
+    ]
+
+    query = (
+        (db.customers_classcards.Enddate >= valid_on) &
+        (db.customers_classcards.ClassesTaken < db.school_classcards.Classes)
+    )
+
+    nr_cards_updated = 0
+    rows = db(query).select(db.customers_classcards.ALL)
+
+    for row in rows:
+        row.Enddate = row.Enddate - datetime.timedelta(days=int(days_to_subtract))
+        row.update_record()
+
+        nr_cards_updated += 1
+
+    return "Subtracted the expiration date for %s cards" % nr_cards_updated
